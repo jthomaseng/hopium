f1467e08ca537796735124251a5db119f29196b0
Jason Thomas <jason.thomas.eng@gmail.com>
2023-09-08 21:52:09 -0700

    Add java. Tweak Header. Start Main

---
+++ loadgamepage.js
@@ -0,0 +1,11 @@
+let gameTitle = "Starfield";
+let developer = "Bethesda Game Studios";
+let publisher = "Bethesda Softworks";
+let gameCoverSrc = "./images/game-covers/starfield-2023.png";
+let wikipediaLink = "https://en.wikipedia.org/wiki/Starfield_(video_game)";
+let IGNLink = "https://www.ign.com/games/starfield";
+
+document.getElementById("page-title").innerHTML = "Hopium | " + gameTitle;
+document.getElementById("game-title").innerHTML = gameTitle;
+document.getElementById("developer").innerHTML = developer;
+document.getElementById("gameCover").src = gameCoverSrc;
