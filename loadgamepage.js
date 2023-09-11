1e172d4789df424ea793ee9a073d0ebaa2f1d6d6
Jason Thomas <jason.thomas.eng@gmail.com>
2023-09-11 11:08:20 -0700

    Add review sections. Format All.

--- loadgamepage.js
+++ loadgamepage.js
@@ -7,5 +7,53 @@ let IGNLink = "https://www.ign.com/games/starfield";
 
 document.getElementById("page-title").innerHTML = "Hopium | " + gameTitle;
 document.getElementById("game-title").innerHTML = gameTitle;
-document.getElementById("developer").innerHTML = developer;
+document.getElementById("developer").innerHTML = "Developer: " + developer;
+document.getElementById("publisher").innerHTML = "Publisher: " + publisher;
+
 document.getElementById("gameCover").src = gameCoverSrc;
+
+
+
+
+
+//Apply logical styles
+//adjust rating color based on score
+
+const applyRatingColor = (array) => {
+    for (i=0; i < array.length; i++) {
+        let element = array[i]
+        switch (true) {
+            case (array[i].innerHTML > 7):
+                array[i].style.color = "rgb(0, 255, 0)";
+                break;
+            case (array[i].innerHTML > 3):
+                array[i].style.color = "orange";
+                break;
+            default:
+                array[i].style.color = "red";
+        }
+    }
+}
+
+const applyPatchSymbol = (array) => {
+    for (i=0; i < array.length; i++) {
+        let element = array[i]
+        switch (true) {
+            case (array[i].innerHTML > 7):
+                array[i].innerHTML = "trending_up";
+                break;
+            case (array[i].innerHTML < 4):
+                array[i].innerHTML = "trending_down";
+                break;
+            default:
+                array[i].innerHTML = "trending_flat";
+        }
+    }
+}
+
+let ratingArray = document.getElementsByClassName("rating");
+applyRatingColor(ratingArray);
+
+let patchArray = document.getElementsByClassName("patchRating");
+applyRatingColor(patchArray);
+applyPatchSymbol(patchArray);
